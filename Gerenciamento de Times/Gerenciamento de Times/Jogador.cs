ae709eccc6966a3dfa5d92768a36cce53ffec0db
HenriqueGalli-atomic <henrique.galli@atomicsolutions.com.br>
2020-03-23 20:10:21 -0300

    Criacao de Classes e Lista

---
+++ Gerenciamento de Times/Gerenciamento de Times/Jogador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerenciamento_de_Times
+{
+    class Jogador
+    {
+        private string nome;
+        private string numero;
+        private string posicao;
+
+        public string Nome { get; set; }
+        public string Numero { get; set; }
+        public string Posicao { get; set; }
+    }
+}
---
+++ Gerenciamento de Times/Gerenciamento de Times/Time.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerenciamento_de_Times
+{
+    class Time
+    {
+        private string nomeTime;
+
+        public string NomeTime { get; set; }
+        public string NomeTreinador { get; set; }
+
+        //Lista com 11 Jogadores
+    }
+}
